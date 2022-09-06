adadf3d6c711f0a403d816fe2ed6a8bc14ec097e
YSBINN <dowkdg@naver.com>
2022-09-06 20:48:54 +0900

    Base.ts 및 queries 폴더 예시 파일 추가

---
+++ src/queries/movieDetail/MovieDetailList.ts
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query';
+import { useSetRecoilState } from 'recoil';
+import { IsAuth } from 'src/atoms';
+import adApi from '../../api/ad';
+import errorHandler from '../common';
+
+export default function adListQuery() {
+  const setIsAuth = useSetRecoilState(IsAuth);
+  return useQuery(['adList'], () => adApi.getAdList(), {
+    retry: false,
+    refetchOnWindowFocus: false,
+    cacheTime: 1000 * 60 * 30,
+    onError: (err) => {
+      errorHandler(err, setIsAuth);
+    },
+    onSuccess: (data) => {}
+  });
+}
