2ae91835f0625ec2a8c0cf9ba1eb4dd42a14d31d
이재하 <jhl2619@naver.com>
2022-09-06 20:28:06 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-undef': 'off',
+    'no-useless-escape': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          1,
+          {
+            args: 'none',
+          },
+        ],
+      },
+    },
+  ],
+};
---
+++ src/Routing.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Routing() {
+  return <></>;
+}
+
+export default Routing;
