1135ae81a7868022fc5708da85a60c2e0f79a748
seriparkdev <qkrtpfl9228@gmail.com>
2022-09-07 17:17:47 +0900

    feat: add loading page
    spinner로  로딩 페이지 구현

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,22 +1,29 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Spinner from 'components/spinner/Spinner';
 import Layout from 'components/layout/Layout';
 import HomePage from 'pages/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function Routing() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/now" element={<HomePage />} />
-          <Route path="/upcoming" element={<HomePage />} />
-          <Route path="/top-rated" element={<HomePage />} />
-          <Route path="/movie/detail/:movieId" element={<HomePage />} />
-          <Route path="/search" element={<HomePage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Layout>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/now" element={<HomePage />} />
+              <Route path="/upcoming" element={<HomePage />} />
+              <Route path="/top-rated" element={<HomePage />} />
+              <Route path="/movie/detail/:movieId" element={<HomePage />} />
+              <Route path="/search" element={<HomePage />} />
+            </Routes>
+          </Suspense>
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default Routing;
+
