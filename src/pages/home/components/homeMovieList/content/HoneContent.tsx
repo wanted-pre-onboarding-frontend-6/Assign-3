19e97a76f3e955eebd10c4f0629d4feb616d43e4
sming0112 <sming0112@gmail.com>
2022-09-07 23:45:54 +0900

    feat: content 컴포넌트(영화 컴포넌트) 생성

--- src/pages/home/components/homeMovieList/content/HoneContent.tsx
+++ src/pages/home/components/homeMovieList/content/HoneContent.tsx
@@ -1,4 +1,64 @@
-const HomeContent = () => {
-  return <div></div>;
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStar);
+
+const IMAGE_URL = process.env.REACT_APP_IMAGE_URL;
+
+const HomeContent = (props: any) => {
+  return (
+    <Container>
+      <PosterWrapper>
+        <img src={IMAGE_URL + props.poster_path} alt="movie-poster" />
+      </PosterWrapper>
+      <DetailContainer>
+        <Title>{props.title}</Title>
+        <VoteAverageContainer>
+          <FontAwesomeIcon icon={['fas', 'star']} />
+          <span>{props.vote_average}</span>
+        </VoteAverageContainer>
+        <Overview>{props.overview}</Overview>
+      </DetailContainer>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 330px;
+  height: 610px;
+  overflow: hidden;
+`;
+
+const PosterWrapper = styled.div`
+  margin-bottom: 4px;
+`;
+
+const DetailContainer = styled.div`
+  padding: 4px;
+`;
+
+const Title = styled.span`
+  color: white;
+  font-weight: 600;
+  font-size: 20px;
+`;
+
+const VoteAverageContainer = styled.div`
+  svg {
+    color: orange;
+  }
+
+  span {
+    margin-left: 4px;
+    color: orange;
+  }
+`;
+
+const Overview = styled.span`
+  font-size: 14px;
+  color: gray;
+`;
+
 export default HomeContent;
