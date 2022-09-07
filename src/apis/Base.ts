9abb87d749a8ddf6e48be14daba0a7534ea69f71
sming0112 <sming0112@gmail.com>
2022-09-07 19:03:13 +0900

    feat: axios 인스턴스 생성, getPopularMovies, getNowPlayingMovies, getUpcomingMovies, getTopRatedMovies, getMovieById api 추가

--- src/apis/Base.ts
+++ src/apis/Base.ts
@@ -1,56 +1,38 @@
-// import axios, { AxiosInstance } from 'axios';
-// import config from '../config/config';
+import axios from 'axios';
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Api {
-  //   private axios: AxiosInstance;
-  //   constructor() {}
-  //   private createAxios(token: string, headerOption?: any) {
-  //     return axios.create({
-  //       baseURL: config.SERVER_URL,
-  //       headers: {
-  //         Accept: 'application/json',
-  //         'Content-Type': 'application/json',
-  //         Authorization: `Bearer ${token}`,
-  //         ...headerOption
-  //       }
-  //     });
-  //   }
-  //   protected async get(
-  //     url: string,
-  //     formData: any,
-  //     token: string,
-  //     headerOption?: any
-  //   ) {
-  //     const axios = this.createAxios(token, headerOption);
-  //     return await axios.get(url, { params: formData });
-  //   }
-  //   protected async post(
-  //     url: string,
-  //     data: any,
-  //     token: string,
-  //     headerOption?: any
-  //   ) {
-  //     const axios = this.createAxios(token, headerOption);
-  //     return await axios.post(url, data);
-  //   }
-  //   protected async put(
-  //     url: string,
-  //     data: any,
-  //     token: string,
-  //     headerOption?: any
-  //   ) {
-  //     const axios = this.createAxios(token, headerOption);
-  //     return await axios.put(url, data);
-  //   }
-  //   protected async delete(
-  //     url: string,
-  //     data: any,
-  //     token: string,
-  //     headerOption?: any
-  //   ) {
-  //     const axios = this.createAxios(token, headerOption);
-  //     return await axios.delete(url, { data });
-  //   }
-}
+  private createAxios(headerOption?: any) {
+    return axios.create({
+      baseURL: BASE_URL,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        ...headerOption,
+      },
+      params: {
+        api_key: API_KEY,
+      },
+    });
+  }
+  protected async get(url: string, params?: any, headerOption?: any) {
+    const axios = this.createAxios(headerOption);
+    return await axios.get(url, { params });
+  }
 
+  protected async post(url: string, data: any, headerOption?: any) {
+    const axios = this.createAxios(headerOption);
+    return await axios.post(url, data);
+  }
+  protected async put(url: string, data: any, headerOption?: any) {
+    const axios = this.createAxios(headerOption);
+    return await axios.put(url, data);
+  }
+  protected async delete(url: string, data: any, headerOption?: any) {
+    const axios = this.createAxios(headerOption);
+    return await axios.delete(url, { data });
+  }
+}
 export default Api;
---
+++ src/apis/MovieApi.ts
@@ -0,0 +1,29 @@
+import Api from './Base';
+
+interface ParamsString {
+  params: string | object;
+  movie_id?: string;
+}
+
+class MovieApi extends Api {
+  public async getPopularMovies({ params }: ParamsString) {
+    return await this.get(`/popular`, { params });
+  }
+
+  public async getNowPlayingMovies({ params }: ParamsString) {
+    return await this.get(`/now_playing`, { params });
+  }
+
+  public async getUpcomingMovies({ params }: ParamsString) {
+    return await this.get(`/upcoming`, { params });
+  }
+
+  public async getTopRatedMovies({ params }: ParamsString) {
+    return await this.get(`/top_rated`, { params });
+  }
+
+  public async getMovieById({ movie_id, params }: ParamsString) {
+    return await this.get(`/${movie_id}`, { params });
+  }
+}
+export default new MovieApi();
